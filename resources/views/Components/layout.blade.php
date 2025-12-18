2653590354cd1fbbbb1cfadd41fed38b2dcd63d5
Denim Datta 👻 <d.dat08@gmail.com>
2025-12-17 20:10:58 -0800

    Add Icon

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>{{ config('app.name', 'Job Board') }}</title>
+        <link rel="icon" href="{{ asset('jobboard_512.ico') }}" type="image/x-icon">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
