82bbc555e9be8cc06ec4377865b800e60a54ce53
Denim Datta 👻 <d.dat08@gmail.com>
2025-12-21 02:51:19 -0800

    Style Currently Active Nav Link

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,3 +1,7 @@
 <x-layout>
+    <x-slot name="pageTitle">
+        About Page
+    </x-slot>
+
     <h1>About Me</h1>
 </x-layout>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,7 @@
 <x-layout>
+    <x-slot name="pageTitle">
+        Home Page
+    </x-slot>
+
     <h1>Denim Datta</h1>
 </x-layout>
