65bea72b4041bdf2c4a085a9726e5edf1b599a71
Denim Datta 👻 <d.dat08@gmail.com>
2025-12-21 03:30:17 -0800

    Contact Page

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,11 @@
+<x-layout>
+    <x-slot name="pageTitle">
+        Contact
+    </x-slot>
+
+    <h1>Contact Details</h1>
+    <p>Email: </p>
+    <p>Phone: </p>
+    <p>Address: </p>
+    <p>Social Media: </p>
+</x-layout>
