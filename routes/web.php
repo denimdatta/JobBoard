019a0838b9ce5b942c385008daa6268784975c7c
Denim Datta 👻 <d.dat08@gmail.com>
2025-12-21 23:10:23 -0800

    Move Jobs into Array

--- routes/web.php
+++ routes/web.php
@@ -3,59 +3,39 @@
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
+$jobs = [
+    [
+        'id' => 1,
+        'title' => 'Software Engineer',
+        'salary' => '$80,000',
+        'location' => 'New York'
+    ],
+    [
+        'id' => 2,
+        'title' => 'Product Manager',
+        'salary' => '$95,000',
+        'location' => 'San Francisco'
+    ],
+    [
+        'id' => 3,
+        'title' => 'UX Designer',
+        'salary' => '$75,000',
+        'location' => 'Remote'
+    ]
+];
+
 Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/jobs', function () {
+Route::get('/jobs', function () use ($jobs) {
     return view('jobs', [
-        'jobs' => [
-            [
-                'id' => 1,
-                'title' => 'Software Engineer',
-                'salary' => '$80,000',
-                'location' => 'New York'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Product Manager',
-                'salary' => '$95,000',
-                'location' => 'San Francisco'
-            ],
-            [
-                'id' => 3,
-                'title' => 'UX Designer',
-                'salary' => '$75,000',
-                'location' => 'Remote'
-            ]
-        ]
+        'jobs' => $jobs,
     ]);
 });
 
-Route::get('/jobs/{id}', function ($id) {
-    $jobs = [
-        [
-            'id' => 1,
-            'title' => 'Software Engineer',
-            'salary' => '$80,000',
-            'location' => 'New York'
-        ],
-        [
-            'id' => 2,
-            'title' => 'Product Manager',
-            'salary' => '$95,000',
-            'location' => 'San Francisco'
-        ],
-        [
-            'id' => 3,
-            'title' => 'UX Designer',
-            'salary' => '$75,000',
-            'location' => 'Remote'
-        ]
-    ];
-
+Route::get('/jobs/{id}', function ($id) use ($jobs) {
     $job = Arr::first($jobs, fn($job) => $job['id'] === (int)$id);
-
     return view('job', ['job' => $job ?? 'Job not found']);
 });
 
