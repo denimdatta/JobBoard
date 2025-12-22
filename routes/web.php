35d67e7b95ad931f5145236047e4bbba8244ff8e
Denim Datta 👻 <d.dat08@gmail.com>
2025-12-21 23:15:24 -0800

    Move Jobs into Class

--- routes/web.php
+++ routes/web.php
@@ -3,39 +3,44 @@
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
-$jobs = [
-    [
-        'id' => 1,
-        'title' => 'Software Engineer',
-        'salary' => '$80,000',
-        'location' => 'New York'
-    ],
-    [
-        'id' => 2,
-        'title' => 'Product Manager',
-        'salary' => '$95,000',
-        'location' => 'San Francisco'
-    ],
-    [
-        'id' => 3,
-        'title' => 'UX Designer',
-        'salary' => '$75,000',
-        'location' => 'Remote'
-    ]
-];
+class Job {
+    public static function getAllJobs(): array
+    {
+        return [
+            [
+                'id' => 1,
+                'title' => 'Software Engineer',
+                'salary' => '$80,000',
+                'location' => 'New York'
+            ],
+            [
+                'id' => 2,
+                'title' => 'Product Manager',
+                'salary' => '$95,000',
+                'location' => 'San Francisco'
+            ],
+            [
+                'id' => 3,
+                'title' => 'UX Designer',
+                'salary' => '$75,000',
+                'location' => 'Remote'
+            ]
+        ];
+    }
+}
 
 Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/jobs', function () use ($jobs) {
+Route::get('/jobs', function () {
     return view('jobs', [
-        'jobs' => $jobs,
+        'jobs' => Job::getAllJobs(),
     ]);
 });
 
-Route::get('/jobs/{id}', function ($id) use ($jobs) {
-    $job = Arr::first($jobs, fn($job) => $job['id'] === (int)$id);
+Route::get('/jobs/{id}', function ($id) {
+    $job = Arr::first(Job::getAllJobs(), fn($job) => $job['id'] === (int)$id);
     return view('job', ['job' => $job ?? 'Job not found']);
 });
 
