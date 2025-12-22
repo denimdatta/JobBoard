e9ae9454a2cfb031cb2f1ebdfec3ea7a7c21b13e
Denim Datta 👻 <d.dat08@gmail.com>
2025-12-21 23:31:49 -0800

    Move Job Class to Model

--- routes/web.php
+++ routes/web.php
@@ -1,33 +1,8 @@
 <?php
 
+use App\Models\Job;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
-
-class Job {
-    public static function getAllJobs(): array
-    {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Software Engineer',
-                'salary' => '$80,000',
-                'location' => 'New York'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Product Manager',
-                'salary' => '$95,000',
-                'location' => 'San Francisco'
-            ],
-            [
-                'id' => 3,
-                'title' => 'UX Designer',
-                'salary' => '$75,000',
-                'location' => 'Remote'
-            ]
-        ];
-    }
-}
 
 Route::get('/', function () {
     return view('home');
