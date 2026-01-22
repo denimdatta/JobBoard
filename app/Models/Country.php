5230782f8cd0cecd6407155be8191a2e181f1fa5
Denim Datta 👻 <d.dat08@gmail.com>
2026-01-22 00:30:27 -0800

    PHP Doc for Models

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -4,6 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property int $id
+ * @property string $iso_code
+ * @property string $name
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Country newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Country newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Country query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Country whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Country whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Country whereIsoCode($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Country whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Country whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Country extends Model
 {
     //
--- app/Models/JobListing.php
+++ app/Models/JobListing.php
@@ -5,6 +5,29 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * @property int $id
+ * @property string $title
+ * @property string $location
+ * @property string $currency_code
+ * @property int $salary Salary in Low Denomination (Cent or Paisa
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property string $country_code
+ * @property-read \App\Models\Country $country
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|JobListing newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|JobListing newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|JobListing query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|JobListing whereCountryCode($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|JobListing whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|JobListing whereCurrencyCode($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|JobListing whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|JobListing whereLocation($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|JobListing whereSalary($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|JobListing whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|JobListing whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class JobListing extends Model
 {
     //
