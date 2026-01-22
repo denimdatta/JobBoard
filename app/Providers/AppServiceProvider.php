8c1ecffe6c33ae223e0b7d9f3853fd1de53aed14
Denim Datta 👻 <d.dat08@gmail.com>
2026-01-21 18:33:19 -0800

    PHP Doc

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,15 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+/**
+ * Service provider for the Job Board application.
+ *
+ * This provider is used to register and bootstrap any
+ * application services required for the Laravel application.
+ *
+ * The application leverages PostgreSQL as the database system
+ * and uses database queue connections for job processing.
+ */
 class AppServiceProvider extends ServiceProvider
 {
     /**
