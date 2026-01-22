d20e3fe21e7a64aeba35936da847dc8c875a1df8
Denim Datta 👻 <d.dat08@gmail.com>
2026-01-22 00:28:05 -0800

    Add Relationship in Job Listing Model
    Add relationship between the JobListing and the Country model.

--- app/Models/JobListing.php
+++ app/Models/JobListing.php
@@ -3,8 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class JobListing extends Model
 {
     //
+    public function country(): BelongsTo
+    {
+        return $this->belongsTo(Country::class, 'country_code', 'iso_code');
+    }
 }
