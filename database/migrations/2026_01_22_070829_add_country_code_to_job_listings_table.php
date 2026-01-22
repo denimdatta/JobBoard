ae4afb6bcae9ba4a5823e79d321de06dee101c1c
Denim Datta 👻 <d.dat08@gmail.com>
2026-01-22 00:25:22 -0800

    Add Migration File to Add Country Code in  Job Listing

---
+++ database/migrations/2026_01_22_070829_add_country_code_to_job_listings_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('job_listings', function (Blueprint $table) {
+            $table->string('country_code', 2)->default('CA');
+
+            $table->foreign('country_code')
+                ->references('iso_code')
+                ->on('countries')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('job_listings', function (Blueprint $table) {
+            $table->dropForeign(['country_code']);
+
+            $table->dropColumn('country_code');
+        });
+    }
+};
