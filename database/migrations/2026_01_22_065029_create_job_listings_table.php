f74b20afffdd03cd32a20a41e2786d9f4d71d09e
Denim Datta 👻 <d.dat08@gmail.com>
2026-01-22 00:03:35 -0800

    Add Migration File to Create Job Listings Table

---
+++ database/migrations/2026_01_22_065029_create_job_listings_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('job_listings', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('location');
+            $table->string('currency_code', 3)->default('CAD');
+            $table->unsignedInteger('salary')->comment('Salary in Low Denomination (Cent or Paisa');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('job_listings');
+    }
+};
