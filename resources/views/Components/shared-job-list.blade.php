63fb8e7c5dc6c7bec2102063f8573631299996a3
Denim Datta 👻 <d.dat08@gmail.com>
2026-01-26 01:51:41 -0800

    Add Blade Component for Jobs List

---
+++ resources/views/Components/shared-job-list.blade.php
@@ -0,0 +1,13 @@
+@props(['linkClass' => '', 'jobs' => ''])
+
+<div class="space-y-4">
+    @foreach($jobs as $job)
+        <a href="/jobs/{{ $job['id'] }}" class="block px-4 py-6 border rounded-lg {{ $linkClass }}">
+            <strong>{{ $job['title'] }}</strong>
+            : pays
+            <i>{{ $job['currency_code'] }} {{ $job['salary'] / 100 }}</i>
+            in
+            <u>{{ $job['location'] }}, {{ $job['country_code'] }}</u>
+        </a>
+    @endforeach
+</div>
