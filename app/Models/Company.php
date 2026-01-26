19fd76b5b1d6629bc9730be005a48786dfe23758
Denim Datta 👻 <d.dat08@gmail.com>
2026-01-25 19:45:13 -0800

    Update Models

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -15,14 +15,14 @@ use Illuminate\Support\Carbon;
  * @property  Carbon|null  $created_at
  * @property  Carbon|null  $updated_at
  *
- * @method  static  CompanyFactory           factory($count = null, $state = [])
- * @method  static  Builder<static>|Company  newModelQuery()
- * @method  static  Builder<static>|Company  newQuery()
- * @method  static  Builder<static>|Company  query()
- * @method  static  Builder<static>|Company  whereCreatedAt($value)
- * @method  static  Builder<static>|Company  whereId($value)
- * @method  static  Builder<static>|Company  whereName($value)
- * @method  static  Builder<static>|Company  whereUpdatedAt($value)
+ * @method  static CompanyFactory  factory($count = null, $state = [])
+ * @method  static  Builder<static>|Company             newModelQuery()
+ * @method  static  Builder<static>|Company             newQuery()
+ * @method  static  Builder<static>|Company             query()
+ * @method  static  Builder<static>|Company             whereCreatedAt($value)
+ * @method  static  Builder<static>|Company             whereId($value)
+ * @method  static  Builder<static>|Company             whereName($value)
+ * @method  static  Builder<static>|Company             whereUpdatedAt($value)
  *
  * @mixin Eloquent
  */
--- app/Models/JobListing.php
+++ app/Models/JobListing.php
@@ -13,28 +13,29 @@ use Illuminate\Support\Carbon;
 /**
  * @property  int          $id
  * @property  string       $title
- * @property  string       $company
+ * @property  int          $company_id
  * @property  string       $location
  * @property  string       $country_code
  * @property  string       $currency_code
  * @property  int          $salary         Salary in Low Denomination (Cent or Paisa
  * @property  Carbon|null  $created_at
  * @property  Carbon|null  $updated_at
+ * @property-read Company $company
  * @property-read Country $country
  *
- * @method  static  JobListingFactory           factory($count = null, $state = [])
- * @method  static  Builder<static>|JobListing  newModelQuery()
- * @method  static  Builder<static>|JobListing  newQuery()
- * @method  static  Builder<static>|JobListing  query()
- * @method  static  Builder<static>|JobListing  whereCompany($value)
- * @method  static  Builder<static>|JobListing  whereCountryCode($value)
- * @method  static  Builder<static>|JobListing  whereCreatedAt($value)
- * @method  static  Builder<static>|JobListing  whereCurrencyCode($value)
- * @method  static  Builder<static>|JobListing  whereId($value)
- * @method  static  Builder<static>|JobListing  whereLocation($value)
- * @method  static  Builder<static>|JobListing  whereSalary($value)
- * @method  static  Builder<static>|JobListing  whereTitle($value)
- * @method  static  Builder<static>|JobListing  whereUpdatedAt($value)
+ * @method  static  JobListingFactory  factory($count = null, $state = [])
+ * @method  static  Builder<static>|JobListing             newModelQuery()
+ * @method  static  Builder<static>|JobListing             newQuery()
+ * @method  static  Builder<static>|JobListing             query()
+ * @method  static  Builder<static>|JobListing             whereCompanyId($value)
+ * @method  static  Builder<static>|JobListing             whereCountryCode($value)
+ * @method  static  Builder<static>|JobListing             whereCreatedAt($value)
+ * @method  static  Builder<static>|JobListing             whereCurrencyCode($value)
+ * @method  static  Builder<static>|JobListing             whereId($value)
+ * @method  static  Builder<static>|JobListing             whereLocation($value)
+ * @method  static  Builder<static>|JobListing             whereSalary($value)
+ * @method  static  Builder<static>|JobListing             whereTitle($value)
+ * @method  static  Builder<static>|JobListing             whereUpdatedAt($value)
  *
  * @mixin Eloquent
  */
@@ -57,7 +58,7 @@ class JobListing extends Model
      */
     public function company(): BelongsTo
     {
-        return $this->belongsTo(Company::class, 'company', 'name');
+        return $this->belongsTo(Company::class);
     }
 
     /**
