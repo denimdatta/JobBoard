c0ff07f52ce61e06e44d9519a4e0d90cc0fb8365
Denim Datta 👻 <d.dat08@gmail.com>
2026-01-25 23:57:26 -0800

    Add Link to Company Jobs Page

--- resources/views/job.blade.php
+++ resources/views/job.blade.php
@@ -7,7 +7,12 @@
     <ul>
         <li><b>Salary:</b> <i>{{ $job['currency_code'] }}</i> {{ $job['salary'] / 100 }}</li>
         <li><b>Location:</b> {{ $job['location'] }}, {{ $job['country_code'] }}</li>
-        <li><b>Company:</b> {{ $job['company']['name'] }}</li>
+        <li>
+            <b>Company:</b>
+            <a href="/jobs/company/{{ $job['company']['id'] }}" class="text-blue-500 hover:underline">
+                {{ $job['company']['name'] }}
+            </a>
+        </li>
         <li><b>Job ID:</b> {{ $job['id'] }}</li>
     </ul>
 </x-layout>
