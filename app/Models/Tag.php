154fb48cdb04af9199abf4cb46e2bdf6158ad1f6
Denim Datta 👻 <d.dat08@gmail.com>
2026-01-25 20:32:51 -0800

    Add PhpDoc on Tag Model

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -2,11 +2,32 @@
 
 namespace App\Models;
 
+use Database\Factories\TagFactory;
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
+/**
+ * @property  int          $id
+ * @property  string       $name
+ * @property  Carbon|null  $created_at
+ * @property  Carbon|null  $updated_at
+ *
+ * @method  static  TagFactory           factory($count = null, $state = [])
+ * @method  static  Builder<static>|Tag  newModelQuery()
+ * @method  static  Builder<static>|Tag  newQuery()
+ * @method  static  Builder<static>|Tag  query()
+ * @method  static  Builder<static>|Tag  whereCreatedAt($value)
+ * @method  static  Builder<static>|Tag  whereId($value)
+ * @method  static  Builder<static>|Tag  whereName($value)
+ * @method  static  Builder<static>|Tag  whereUpdatedAt($value)
+ *
+ * @mixin Eloquent
+ */
 class Tag extends Model
 {
-    /** @use HasFactory<\Database\Factories\TagFactory> */
+    /** @use HasFactory<TagFactory> */
     use HasFactory;
 }
