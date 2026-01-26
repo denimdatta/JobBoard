2ffcc25228fcad3ad09fa108223ba19d8cdd9289
Denim Datta 👻 <d.dat08@gmail.com>
2026-01-26 01:14:56 -0800

    Add Page for Tag Job

---
+++ resources/views/tag_job.blade.php
@@ -0,0 +1,19 @@
+<x-layout>
+    <x-slot name="pageTitle">
+        {{ $tag['name'] }} | Jobs Page
+    </x-slot>
+
+    <ul>
+        @foreach($jobs as $job)
+            <li>
+                <a href="/jobs/{{ $job['id'] }}" class="text-blue-500 hover:underline">
+                    <strong>{{ $job['title'] }}</strong>
+                </a>
+                : pays
+                <i>{{ $job['currency_code'] }} {{ $job['salary'] / 100 }}</i>
+                in
+                <u>{{ $job['location'] }}, {{ $job['country_code'] }}</u>
+            </li>
+        @endforeach
+    </ul>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Models\Company;
 use App\Models\JobListing;
+use App\Models\Tag;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -24,6 +25,15 @@ Route::get('/jobs/company/{id}', function ($id) {
     return view('company_job', [
         'company' => Company::query()->find($id),
         'jobs' => JobListing::query()->where('company_id', $id)->get(),
+    ]);
+});
+
+Route::get('/jobs/tag/{id}', function ($id) {
+    $tag = Tag::query()->find($id);
+
+    return view('tag_job', [
+        'tag' => $tag,
+        'jobs' => $tag->jobs,
     ]);
 });
 
